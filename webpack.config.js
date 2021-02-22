d16a488936d1e7d3dfb1a60794ea1fc3b3630cc1
Ssirey <ssirey30zero@gmail.com>
2021-02-22 17:56:27 +0300

    my first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const { HotModuleReplacementPlugin } = require("webpack")
+
+// webpack.config.js
+module.exports = {
+	entry: [
+		'./src/main.js'
+	],
+	output: {
+		filename: 'build.js'
+	},
+	module: {
+		rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+		]
+	}
+};
